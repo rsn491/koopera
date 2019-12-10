786f3cc803799541833ab44f1ddda7d477168fef
rsn491 <rsn491@gmail.com>
2019-12-10 22:46:42 +0100

    Add support for commenting in python files

--- src/backend/code_comment.py
+++ src/backend/code_comment.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from datetime import datetime
 from github.IssueComment import IssueComment
+from github.PullRequestComment import PullRequestComment
 
 
 class CodeComment:
@@ -12,30 +13,44 @@ class CodeComment:
         self.author = author
         self.updatedAt = updated_at
 
-    def to_github_issue_comment_body(self):
-        return "[%s|%s] %s" % (self.filePath, self.codeBlockId, self.body)
+    @classmethod
+    def is_ipynb_file(cls, path):
+        return path.endswith('.ipynb')
 
-    @staticmethod
-    def from_github_issue_comment(issue_comment: IssueComment) -> Optional['CodeComment']:
-        body = issue_comment.body
-        first_separator_index = body.find('|')
-        second_separator_index = body.find(']')
+    def get_github_path(self) -> int:
+        return self.filePath
 
-        if first_separator_index == -1 \
-                or second_separator_index == -1 \
-                or first_separator_index > second_separator_index \
-                or body.__len__() < 3 \
-                or body[0] != '[':
-            return None
+    def get_github_position(self) -> int:
+        if self.is_ipynb_file(self.filePath):
+            return 1
 
-        code_block_id = body[first_separator_index + 1:second_separator_index]
+        return self.codeBlockId + 1
 
-        if code_block_id.isdigit() is False:
-            return None
+    def get_github_comment(self) -> str:
+        if self.is_ipynb_file(self.filePath):
+            return f'[{self.codeBlockId}] {self.body}'
+
+        return self.body
+
+    @classmethod
+    def from_github_code_comment(cls, code_comment: PullRequestComment):
+        body = code_comment.body
+        
+        if cls.is_ipynb_file(code_comment.path):
+            separator_index = body.find(']')
+            code_block_id = body[1: separator_index]
+
+            if code_block_id.isdigit() is False:
+                return None
+
+            code_block_id = int(code_block_id)
+            body = body[separator_index + 2: body.__len__()]
+        else:
+            code_block_id = code_comment.position - 1 if code_comment.position is not None else 1
 
         return CodeComment(
-            file_path=body[1:first_separator_index],
-            code_block_id=int(code_block_id),
-            body=body[second_separator_index + 2:body.__len__()],
-            author=issue_comment.user.name,
-            updated_at=issue_comment.updated_at.timestamp())
+            file_path=code_comment.path,
+            code_block_id=code_block_id,
+            body=body,
+            author=code_comment.user.name,
+            updated_at=code_comment.updated_at.timestamp())
